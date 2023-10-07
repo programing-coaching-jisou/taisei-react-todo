aacabb81f43e183a3354cbcf00f4a7e83ceebdea
Taisei <taisei96.web@gmail.com>
2023-10-07 18:57:32 +0900

    環境構築

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@chakra-ui/react';
+
+function App() {
+
+  return (
+    <>
+      <div>hello world</div>
+      <Button colorScheme="blue">Button</Button>
+    </>
+  );
+}
+
+export default App
